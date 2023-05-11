eaa352e65b77d4a1700cd6edb5deb1318a153838
夢路 佐藤 <sato.yumeji@nsg.gr.jp>
2023-05-11 16:07:54 +0900

    第2章練習問題を提出

---
+++ WindowsFormsApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名前";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonOK.Location = new System.Drawing.Point(99, 102);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(91, 23);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = false;
+            this.buttonOK.Click += new System.EventHandler(this.button1_Click);
+            this.buttonOK.MouseEnter += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(56, 12);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(105, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelMessage.Location = new System.Drawing.Point(28, 51);
+            this.labelMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(114, 20);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "あいうえお";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 147);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "こんにちはC#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ WindowsFormsApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 画面が表示された時のイベント
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelMessage.Text = "";
+            // ラジオボタン１の値をラベル2に表示
+            // ラジオボタン２の値をラベル3に表示
+            // ニューメリックアップダウンの値をラベル4に表示
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            labelMessage.Text = textBoxName.Text 
+                + "さん、こんにちは。";
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+   
+        }
+    }
+}
